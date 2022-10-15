2083a6c0e71ea029ed91e6708a5060a4286f4f77
ramaherdianto <ramadhanherdianto35@gmail.com>
2022-10-15 08:29:59 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App flex flex-col justify-center items-center">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button className="py-3 px-8 bg-sky-400 text-white rounded-[30px]" onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <button className="py-3 px-5 bg-sky-400 text-white">Tailwind CSS</button>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+    </div>
+  );
+}
+
+export default App;
