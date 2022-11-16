35b3a747f65e78cac560424c554e5f7aaeb42798
ramaherdianto <ramadhanherdianto35@gmail.com>
2022-11-16 20:38:34 +0700

    learn react component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
+import { FaFacebookF, FaGithubAlt } from "react-icons/fa";
+import { GrMail } from "react-icons/gr";
 import "./App.css";
 
 function App() {
@@ -26,8 +28,37 @@ function App() {
       </div>
       <button className="py-3 px-5 bg-sky-400 text-white">Tailwind CSS</button>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+
+      <div className="flex mt-7 gap-2">
+        <Button type="button" onClick={() => console.log("Ragister dengan Facebook")}>
+          <FaFacebookF className="w-5 h-5 stroke-1" />
+          Facebook
+        </Button>
+        <Button className="bg-red-500" type="submit" onClick={() => console.log("Login dengan Email")}>
+          <GrMail />
+          Email
+        </Button>
+        <Button className="bg-transparent border border-slate-700 text-black" type="button" onClick={() => console.log("Ragister dengan Github")}>
+          <FaGithubAlt className="w-5 h-5 stroke-1" />
+          Github
+        </Button>
+        {/* <button className="bg-red-400 text-white px-4 py-2 rounded">Gmail</button>
+        <button className="bg-slate-700 text-white px-4 py-2 rounded">Github</button> */}
+      </div>
     </div>
   );
 }
+
+const Button = (props) => {
+  const { className = "bg-blue-500", text, children } = props;
+
+  return (
+    <>
+      <button {...props} className={`${className} flex gap-x-2 items-center text-white px-4 py-2 rounded`}>
+        {text || children}
+      </button>
+    </>
+  );
+};
 
 export default App;
